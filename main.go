8d8aa2d2f15716fc5eb482b02c3aa0d8053e8414
kambe <nori.k3636@gmail.com>
2020-07-04 21:28:19 +0900

    エラーを探すためにコメントアウト

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	// ビルド時のみ使用する
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -27,54 +26,78 @@ func main() {
 		log.Fatal(err)
 	}
 
-	insertData(DbConnection)
+	//insertData(DbConnection)
+	// //getRuleData(DbConnection)
+	// cmd := "SELECT * FROM Rule"
+	// rows, _ := DbConnection.Query(cmd)
 
-	cmd := "SELECT * FROM tournament"
-	rows, _ := DbConnection.Query(cmd)
+	// // データ保存領域を確保
+	// var rule []Rule
+	// for rows.Next() {
+	// 	var r Rule
+	// 	// Scan にて、struct のアドレスにデータを入れる
+	// 	err := rows.Scan(&r.regulation, &r.league)
+	// 	// エラーハンドリング(共通関数にした方がいいのかな)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	// データ取得
+	// 	rule = append(rule, r)
+	// }
 
-	// データ保存領域を確保
-	var tournaments []Tournament
-	for rows.Next() {
-		var t Tournament
-		// Scan にて、struct のアドレスにデータを入れる
-		err := rows.Scan(&t.ID, &t.name, &t.regulation)
-		// エラーハンドリング(共通関数にした方がいいのかな)
-		if err != nil {
-			log.Println(err)
-		}
-		// データ取得
-		tournaments = append(tournaments, t)
-	}
+	// // 操作結果を確認
+	// for _, r := range rule {
+	// 	fmt.Println(r.regulation, r.league)
+	// }
 
-	// 操作結果を確認
-	for _, t := range tournaments {
-		fmt.Println(t.ID, t.name, t.regulation)
-	}
+	// defer rows.Close()
 
-	defer rows.Close()
+	// cmd := "SELECT * FROM tournament"
+	// rows, _ := DbConnection.Query(cmd)
 
-	cmd = "SELECT * FROM player"
-	playerRows, _ := DbConnection.Query(cmd)
+	// // データ保存領域を確保
+	// var tournaments []Tournament
+	// for rows.Next() {
+	// 	var t Tournament
+	// 	// Scan にて、struct のアドレスにデータを入れる
+	// 	err := rows.Scan(&t.ID, &t.name, &t.regulation)
+	// 	// エラーハンドリング(共通関数にした方がいいのかな)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	// データ取得
+	// 	tournaments = append(tournaments, t)
+	// }
 
-	defer playerRows.Close()
+	// // 操作結果を確認
+	// for _, t := range tournaments {
+	// 	fmt.Println(t.ID, t.name, t.regulation)
+	// }
 
-	// データ保存領域を確保
-	var players []Player
-	for playerRows.Next() {
-		var p Player
-		// Scan にて、struct のアドレスにデータを入れる
-		err := playerRows.Scan(&p.ID, &p.name, &p.twitter, &p.blog)
-		// エラーハンドリング(共通関数にした方がいいのかな)
-		if err != nil {
-			log.Println(err)
-		}
-		// データ取得
-		players = append(players, p)
-	}
+	// defer rows.Close()
 
-	// 操作結果を確認
-	for _, p := range players {
-		fmt.Println(p.ID, p.name, p.twitter, p.blog)
-	}
+	// cmd = "SELECT * FROM player"
+	// playerRows, _ := DbConnection.Query(cmd)
+
+	// defer playerRows.Close()
+
+	// // データ保存領域を確保
+	// var players []Player
+	// for playerRows.Next() {
+	// 	var p Player
+	// 	// Scan にて、struct のアドレスにデータを入れる
+	// 	err := playerRows.Scan(&p.ID, &p.name, &p.twitter, &p.blog)
+	// 	// エラーハンドリング(共通関数にした方がいいのかな)
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	// データ取得
+	// 	players = append(players, p)
+	// }
+
+	// // 操作結果を確認
+	// for _, p := range players {
+	// 	fmt.Println(p.ID, p.name, p.twitter, p.blog)
+	// }
 
 }
